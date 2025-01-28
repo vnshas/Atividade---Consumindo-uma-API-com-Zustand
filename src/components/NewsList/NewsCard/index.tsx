eeb784ea3853dedc9e0b61b3f24a8a749681d452
vnshas <163927528+vnshas@users.noreply.github.com>
2025-01-28 08:24:46 -0300

    final

--- src/components/NewsList/NewsCard/index.tsx
+++ src/components/NewsList/NewsCard/index.tsx
@@ -9,7 +9,7 @@ export const NewsCard = ({news}: InewsCardProps) => {
     
     return(
         <li key={news.id}>
-        <span>{news.category}</span>
+        <span>{news.category} - {news.author}</span>
         <h3>{news.title}</h3>
         <p>{news.content}</p>
         </li>
